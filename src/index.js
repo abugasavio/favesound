6eddf3cc6efa90e3fcfe013a2463ef538a39dff6
Savio Abuga <savioabuga@gmail.com>
2017-03-30 16:17:02 +0300

    setting up eslint

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
+/* global document,module */
+
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
 
 const title = 'My Favesound App';
 
 ReactDOM.render(
-    <div>{title}</div>,
-    document.getElementById('app')
+  <div>{title}</div>,
+  document.getElementById('app'),
 );
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,31 @@
 module.exports = {
-    entry: [
-        'webpack-dev-server/client?http://localhost:8080',
-        'webpack/hot/only-dev-server',
-        './src/index.js'
-    ],
-    output: {
-        path: __dirname + '/dist',
-        publicPath: '/',
-        filename: 'bundle.js'
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './src/index.js',
+  ],
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'react-hot-loader!babel-loader',
     },
-    module: {
-        loaders: [{
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            loader: 'react-hot-loader!babel-loader'
-        },
-        {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loaders: ['babel-loader', 'eslint-loader']
-        }]
-    },
-    resolve: {
-        extensions: ['*', '.js', '.jsx']
-    },
-    devServer: {
-        contentBase: './dist',
-        hot: true
-    }
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loaders: ['babel-loader', 'eslint-loader'],
+    }],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+  },
 };
